619654f7fa527087ed46ef996dcd272e857b311b
PuffCab <berlinkollner@gmail.com>
2023-06-16 15:24:48 +0200

    vercel client url included in allowed origins

--- server/index.js
+++ server/index.js
@@ -21,21 +21,21 @@ const addMiddlewares = () => {
       extended: true,
     })
   );
-  var allowCrossDomain = function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  };
+  // var allowCrossDomain = function (req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  //   res.header(
+  //     "Access-Control-Allow-Headers",
+  //     "Origin, X-Requested-With, Content-Type, Accept"
+  //   );
+  //   next();
+  // };
 
-  app.use(allowCrossDomain);
+  // app.use(allowCrossDomain);
 
   const allowedOrigins = [
     "http://localhost:5174",
-    "https://deploy-kangaroos-client.netlify.app",
+    "https://mern-deploy-vercel-client.vercel.app",
   ];
   const corsOptions = {
     origin: function (origin, callback) {
@@ -47,8 +47,8 @@ const addMiddlewares = () => {
     },
   };
 
-  app.use(cors());
-  // app.use(cors(corsOptions));
+  // app.use(cors());
+  app.use(cors(corsOptions));
   cloudinaryConfig();
 
   app.use(passport.initialize());
