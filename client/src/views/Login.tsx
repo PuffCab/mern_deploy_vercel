a025423f9d3443b820f93cbfb6a2a36e9480344a
PuffCab <berlinkollner@gmail.com>
2023-07-20 12:17:16 +0200

    remote server url

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -52,17 +52,17 @@ const Login = () => {
 
     try {
       //!fetching ONLY with local host
-      const response = await fetch(
-        "http://localhost:5001/api/users/login",
-        requestOptions
-      );
+      // const response = await fetch(
+      //   "http://localhost:5001/api/users/login",
+      //   requestOptions
+      // );
 
       //REVIEW[epic=deploy, seq=2] 2-Once we deployed the server, we fetch the data from our API using vercel's URL+Endpoint
       //! fetching ONLY with deployed (server) URL
-      // const response = await fetch(
-      //   "https://mern-deploy-vercel-server.vercel.app/api/users/login",
-      //   requestOptions
-      // );
+      const response = await fetch(
+        "https://mern-deploy-vercel-server.vercel.app/api/users/login",
+        requestOptions
+      );
 
       //! Fetching with either local or deployed URL.
       // const response = await fetch(
