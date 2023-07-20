fdae8674334be7d6390a876f8f06c168d31a366b
PuffCab <berlinkollner@gmail.com>
2023-07-20 10:47:30 +0200

    removed cors options

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,7 +1,7 @@
-//REVIEW[epic=deploy, seq=1] Get rid of all errors and warnings (unused import of react in ln2, unused state var in ln 23)
+//REVIEW[epic=deploy, seq=1] 1-Get rid of all errors and warnings (unused import of react in ln2, unused state var in ln 23)
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { checkUserStatus } from "../utils/checkUserStatus";
-import { serverURL } from "../utils/serverURL";
+// import { serverURL } from "../utils/serverURL";
 
 type User = {
   userName: string;
@@ -53,12 +53,12 @@ const Login = () => {
 
     try {
       //!fetching ONLY with local host
-      // const response = await fetch(
-      //   "http://localhost:5001/api/users/login",
-      //   requestOptions
-      // );
+      const response = await fetch(
+        "http://localhost:5001/api/users/login",
+        requestOptions
+      );
 
-      //REVIEW[epic=deploy, seq=5] Once we deployed the server, we fetch the data from our API using vercel's URL+Endpoint
+      //REVIEW[epic=deploy, seq=5] 5-Once we deployed the server, we fetch the data from our API using vercel's URL+Endpoint
       //! fetching ONLY with deployed (server) URL
       // const response = await fetch(
       //   "https://mern-deploy-vercel-server.vercel.app/api/users/login",
@@ -66,10 +66,10 @@ const Login = () => {
       // );
 
       //! Fetching with either local or deployed URL.
-      const response = await fetch(
-        `${serverURL}/api/users/login`,
-        requestOptions
-      );
+      // const response = await fetch(
+      //   `${serverURL}/api/users/login`,
+      //   requestOptions
+      // );
       console.log("response", response);
 
       if (response.ok) {
