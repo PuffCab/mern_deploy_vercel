ad6222e46a2a1d8f71b13d958c8cb041490f6a1b
PuffCab <berlinkollner@gmail.com>
2023-07-20 10:55:39 +0200

    list of allowed origins implemented

--- server/index.js
+++ server/index.js
@@ -36,25 +36,25 @@ const addMiddlewares = () => {
   //REVIEW[epic=deploy, seq=2] 2-once the client is deployed we can add the URL to the list of allowed Origins
 
   //REVIEW[epic=deploy, seq=3] 3-the first origin should be the localhost port our client runs on. The second one, vercel's URL for our client
-  // const allowedOrigins = [
-  //   "http://localhost:5174",
-  //   // "http://localhost:5001",
-  //   "https://mern-deploy-vercel-client.vercel.app",
-  // ];
-  // const corsOptions = {
-  //   origin: function (origin, callback) {
-  //     // !origin will allow to accept direct calls to the api , with no heading, e.g. http://localhost:5001/api/cities/all
-  //     //REVIEW[epic=deploy, seq=7] 7- !origin will allow requests with no header (origin===undefined), the direct ones (using directly the server url). This solution will now accept only request from those 2 origins, or with no header.
-  //     //Accepting requests with no header might pose a security threat ...research how convinient the solution is.
+  const allowedOrigins = [
+    "http://localhost:5174",
+    // "http://localhost:5001",
+    "https://mern-client-three.vercel.app/",
+  ];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      // !origin will allow to accept direct calls to the api , with no heading, e.g. http://localhost:5001/api/cities/all
+      //REVIEW[epic=deploy, seq=7] 7- !origin will allow requests with no header (origin===undefined), the direct ones (using directly the server url). This solution will now accept only request from those 2 origins, or with no header.
+      //Accepting requests with no header might pose a security threat ...research how convinient the solution is.
 
-  //     // if (allowedOrigins.indexOf(origin) !== -1 ) {
-  //     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error("Not allowed by CORS"));
-  //     }
-  //   },
-  // };
+      // if (allowedOrigins.indexOf(origin) !== -1 ) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
 
   app.use(cors());
   // app.use(cors(corsOptions));
